804ebbb39f577f316d3d7e3b902f5784907aef7e
hopman <88007004508qQ>
2019-10-23 22:34:57 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		
+    <b-container class="bv-example-row pt-3">
+      
+        <Search></Search>
+        
+    </b-container>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Search from "./components/Search.vue";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      Search
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+		<b-row>
+   
+			<b-col cols="10">
+				<b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+				<div class="mt-2">Value: {{ text }}</div>
+			</b-col>
+   
+			<b-col>
+				<b-button block variant="outline-primary">Поиск</b-button>
+			</b-col>
+   
+		</b-row>
+</template>
+
+<script>
+  export default {
+    name: 'Search',
+    data: () => ({
+      text: `Ведите название фильма`
+    })
+  }
+</script>
